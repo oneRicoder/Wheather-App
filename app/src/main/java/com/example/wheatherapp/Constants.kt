01b96559b3666b6c11d0bd6d198e867f0dd3ca0b
oneRicoder <rinkusingh805764@gmail.com>
2022-04-21 23:37:06 +0530

    21st April 2022

---
+++ app/src/main/java/com/example/wheatherapp/Constants.kt
@@ -0,0 +1,28 @@
+package com.example.wheatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants {
+    fun isInternetAvailable(context: Context): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when{
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+
+        }else{
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+
+    }
+}
