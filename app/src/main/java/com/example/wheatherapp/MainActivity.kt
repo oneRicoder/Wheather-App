cb63bbe5402d650e803adcd7d03afa3801d1588f
oneRicoder <rinkusingh805764>
2022-04-23 15:53:58 +0530

    implemented all ui text and images

--- app/src/main/java/com/example/wheatherapp/MainActivity.kt
+++ app/src/main/java/com/example/wheatherapp/MainActivity.kt
@@ -8,12 +8,18 @@ import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.wheatherapp.databinding.ActivityMainBinding
+import com.example.wheatherapp.network.WeatherService
+import com.example.wheatherapp.weather.WeatherResponse
 import com.google.android.gms.location.*
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
@@ -22,16 +28,17 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    //to jst check
-
+    var binding: ActivityMainBinding? = null
     private lateinit var mFusedLocationProvider:FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         mFusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
         if (!isLocationEnabled()){
@@ -76,7 +83,7 @@ class MainActivity : AppCompatActivity() {
             val mLastLocation: Location = locationResult.lastLocation
             val latitude = mLastLocation.latitude
             val longitude = mLastLocation.longitude
-            getLocationWeatherDetail()
+            getLocationWeatherDetail(latitude, longitude)
         }
     }
 
@@ -106,13 +113,95 @@ class MainActivity : AppCompatActivity() {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
-    private fun getLocationWeatherDetail(){
+    private fun getLocationWeatherDetail(longitude: Double, latitude: Double){
         if (Constants.isInternetAvailable(this)){
             //Toast.makeText(this,"You are connected to the internet",Toast.LENGTH_SHORT).show()
             val retrofit: Retrofit = Retrofit.Builder().baseUrl(Constants.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
 
+            val service: WeatherService = retrofit.create<WeatherService>(WeatherService::class.java)
+
+            val listCall: Call<WeatherResponse> = service.getWeather(longitude,latitude,Constants.METRIC_UNIT,Constants.APP_ID)
+            listCall.enqueue(object : Callback<WeatherResponse>{
+                @RequiresApi(Build.VERSION_CODES.N)
+                override fun onResponse(
+                    call: Call<WeatherResponse>,
+                    response: Response<WeatherResponse>
+                ) {
+                    if (response.isSuccessful){
+                        val weatherList: WeatherResponse? = response.body()
+                        Log.i("Response Result","$weatherList")
+                        setUpUi(weatherList!!)
+                    }else{
+                        val rc = response.code()
+                        when(rc){
+                            400 -> Log.i("Error 400","Bad Connection")
+                            404 -> Log.i("Error 404", "Not found")
+                            else -> Log.i("Error", "Generic Error")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                    Log.i("Errror",t.message.toString())
+                }
+
+            })
+
         }else{
             Toast.makeText(this,"No Internet Connection!",Toast.LENGTH_SHORT).show()
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setUpUi(weatherList: WeatherResponse){
+        for (i in weatherList.weather.indices){
+            Log.i("weatherList",weatherList.weather.toString())
+
+            binding?.tvMain?.text = weatherList.weather[i].main
+            binding?.tvMainDescription?.text = weatherList.weather[i].description
+
+            binding?.tvTemp?.text = weatherList.main.temp.toString() + getUnit(application.resources.configuration.locales.toString())
+
+            binding?.tvSunriseTime?.text = unixTime(weatherList.sys.sunrise)
+            binding?.tvSunsetTime?.text = unixTime(weatherList.sys.sunset)
+
+            binding?.tvHumidity?.text = "Humidity " + weatherList.main.humidity.toString() + "%"
+            binding?.tvMin?.text = weatherList.main.temp_min.toString() + "min"
+            binding?.tvMax?.text = weatherList.main.temp_max.toString() + "max"
+            binding?.tvSpeed?.text = weatherList.wind.speed.toString()
+            binding?.tvName?.text = weatherList.name
+            binding?.tvCountry?.text = weatherList.sys.country
+
+            when(weatherList.weather[i].icon){
+                "01d" -> binding?.ivMain?.setImageResource(R.drawable.sunny)
+                "02d" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "03d" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "04d" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "04n" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "10d" -> binding?.ivMain?.setImageResource(R.drawable.rain)
+                "11d" -> binding?.ivMain?.setImageResource(R.drawable.storm)
+                "13d" -> binding?.ivMain?.setImageResource(R.drawable.snowflake)
+                "01n" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "02n" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "03n" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "10n" -> binding?.ivMain?.setImageResource(R.drawable.cloud)
+                "11n" -> binding?.ivMain?.setImageResource(R.drawable.rain)
+                "13n" -> binding?.ivMain?.setImageResource(R.drawable.snowflake)
+            }
+
+        }
+    }
+    private fun getUnit(value: String): String{
+        var value1 = "°C"
+        if (value == "US" || value == "LR" || value == "MM"){
+            value1 = "°F"
+        }
+        return value1
+    }
+    private fun unixTime(timex: Long): String {
+        val date = Date(timex *1000L)
+        val sdf = SimpleDateFormat("hh:mm",Locale("hi","IN"))
+        sdf.timeZone = TimeZone.getDefault()
+        return sdf.format(date)
     }
 }
--- app/src/main/java/com/example/wheatherapp/network/WeatherService.kt
+++ app/src/main/java/com/example/wheatherapp/network/WeatherService.kt
@@ -1,7 +1,16 @@
 package com.example.wheatherapp.network
 
+import com.example.wheatherapp.weather.WeatherResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherService {
-    //@GET("")
+    @GET("2.5/weather")
+    fun getWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("units") units: String,
+        @Query("appid") appid: String?,
+    ) : Call<WeatherResponse>
 }
